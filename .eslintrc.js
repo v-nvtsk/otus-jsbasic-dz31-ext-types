5f72c483ecf57e4c479664a842cabe832136cbde
v-nvtsk <v@nvtsk.ru>
2024-01-28 22:20:39 +0300

    build: add config files

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@stylistic/eslint-plugin-ts"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.eslint.json", "./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    indent: ["error", 2],
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": "off",
+    "comma-style": ["error", "last"],
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        comments: 120,
+        tabWidth: 2,
+        ignoreUrls: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+  ignorePatterns: ["node_modules", ".git", "coverage", "dist"],
+};
